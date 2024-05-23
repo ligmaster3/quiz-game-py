b6fdd4bdb584e2022f29750f38d1f5e469ac6477
ligmaster <136215596+ligmaster3@users.noreply.github.com>
2024-05-22 23:47:44 -0600

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,59 @@
+#quiz_data
+
+quiz_data = questions_player1 + questions_player2
+
+questions_player1 = [
+    {
+        "question": "¿Cuál de estos es el lenguaje de alto nivel?",
+        "choices": ["A. C++", "B. vim", "C. Cobol", "D. P.H.P."],
+        "answer": "D"
+    },
+    {
+        "question": "¿Cuáles son los componentes internos de la computadora?",
+        "choices": ["A. Mouse", "B. Procesador", "C. Teclado", "D. Batería"],
+        "answer": "B"
+    },
+    {
+        "question": "¿Cómo o qué es el sistema binario?",
+        "choices": ["A. Cifra", "B. Entero", "C. Binario", "D. De dos dígitos"],
+        "answer": "D"
+    },
+    {
+        "question": "¿Cuál es el nombre del dueño de Microsoft?",
+        "choices": ["A. Ricardo Blue", "B. Leonardo González", "C. Ricardo Martinelli", "D. Bill Gates"],
+        "answer": "D"
+    },
+    {
+        "question": "¿Transformación del modelo Entidad-Relación?",
+        "choices": ["A. Cardinalidad", "B. Atributos", "C. Cadena", "D. Código"],
+        "answer": "A"
+    }
+]
+
+questions_player2 = [
+    {
+        "question": "¿Cuál es el país más grande del mundo?",
+        "choices": ["A. Estados Unidos", "B. China", "C. Rusia", "D. Canadá"],
+        "answer": "C"
+    },
+    {
+        "question": "¿Qué fue el Holocausto Nazi?",
+        "choices": ["A. Un terremoto", "B. Una masacre", "C. Un tornado", "D. Un arma"],
+        "answer": "B"
+    },
+    {
+        "question": "¿Quién fue Francisco Franco?",
+        "choices": ["A. Poeta chileno", "B. Dictador español", "C. Escritor venezolano", "D. Independentista ecuatoriano"],
+        "answer": "B"
+    },
+    {
+        "question": "¿Quién es el mayor asesino en la historia?",
+        "choices": ["A. Adolf Hitler", "B. Mao Zedong", "C. Iósif Stalin", "D. Ninguno de los anteriores"],
+        "answer": "B"
+    },
+    {
+        "question": "¿Cuál es el libro más vendido de la historia?",
+        "choices": ["A. El Principito", "B. 100 años de soledad", "C. Harry Potter", "D. La Biblia"],
+        "answer": "D"
+    }
+]
